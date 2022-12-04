13213a2d7638eeb2e577c5a96995fdd520af0d4c
Catherine <cpolknytx@gmail.com>
2022-12-04 17:40:02 -0600

    fixed server and user route

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+// router.get('/', (req, res) => {
+//     res.send('Hello, users!');
+// });
+
+// const {
+//     getUsers,
+//     getSingleUser,
+//     createUser,
+// } = require('../../controllers/')
+
+module.exports = router; 
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const cwd = process.swd();
 
 const PORT = process.env.port || 3001;
 const app = express();
@@ -14,6 +13,6 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server for ${activity} running on port ${PORT}!`);
+    console.log(`API server for running on port ${PORT}!`);
   });
 });
